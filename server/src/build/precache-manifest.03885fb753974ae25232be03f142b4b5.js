af66e663e8cf81ccb84d4b64ab96c611166b2be9
Hugo de Melo <hsemog@gmail.com>
2019-09-14 15:50:51 +0200

    release version to aws

---
+++ server/src/build/precache-manifest.03885fb753974ae25232be03f142b4b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed58c9a7f33071f46e07cfd4caf7c064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46d58f79280a31a0701b",
+    "url": "/static/css/main.9f0d67c3.chunk.css"
+  },
+  {
+    "revision": "bcf08db560d419b8e254",
+    "url": "/static/js/2.a6b8efe9.chunk.js"
+  },
+  {
+    "revision": "46d58f79280a31a0701b",
+    "url": "/static/js/main.c63c7db4.chunk.js"
+  },
+  {
+    "revision": "24ff4e312803109d40ff",
+    "url": "/static/js/runtime~main.58fbd735.js"
+  }
+]);
